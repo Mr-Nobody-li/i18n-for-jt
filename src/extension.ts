dd5d2f8bd1bf0cbd0c9c2a5b2339969567bf72b4
lipengfei <lpf13015886562@gmail.com>
2022-04-21 13:52:05 +0800

    test: test

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from "vscode";
 import command from "./command";
 
 export function activate(context: vscode.ExtensionContext) {
+  console.log(`🚀 => vscode`, vscode);
   command.initSetting(context);
   context.subscriptions.push(command.translate);
 }
